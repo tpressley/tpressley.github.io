7b9cfdb572629358b477dd55f0b5ab354b74a3c9
Tristan Pressley <trspres@gmail.com>
2022-08-23 21:40:27 -0400

    inital backend work for merch pages

--- angular-rewrite/noble-syndicate/src/app/app.module.ts
+++ angular-rewrite/noble-syndicate/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MerchandiseComponent } from './merchandise/merchandise.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PublicComponent } from './public/public.component';
+import { MerchItemComponent } from './merch-item/merch-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MerchandiseComponent,
+    PublicComponent,
+    MerchItemComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
 
   ],
   providers: [],
